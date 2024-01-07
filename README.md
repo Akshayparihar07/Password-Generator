fd721dec28de2fb6a71d9bc2b9eb70589635d290
Akshay Parihar <pariharakshay40@gmail.com>
2024-01-07 15:28:55 +0530

    Created password_generator and Added Markdown File

---
+++ README.md
@@ -0,0 +1,30 @@
+# Password Generator
+
+This is a simple password generator written in Python. It allows users to generate random passwords based on their preferences for length, inclusion of numbers, and special characters.
+
+## Features
+
+- Generates random passwords using a combination of letters, numbers, and special characters.
+- Customizable password length.
+- Options to include or exclude numbers and special characters.
+
+## Usage
+
+1. Run the script by executing `python password_generator.py`.
+2. Enter the desired password length when prompted.
+3. Choose whether you want numbers and/or special characters in your password.
+
+## How to Use
+
+- Clone the repository: `git clone https://github.com/Akshayparihar07/password-generator.git`
+- Navigate to the project directory: `cd password-generator`
+- Run the script: `python password_generator.py`
+
+## Example
+
+```bash
+$ python password_generator.py
+Enter a Length for your password: 12
+Do you want your password to have numbers? (Yes / No): Yes
+Do you want your password to have special characters? (Yes / No): Yes
+Generated Password: A9#xH3zL!pQ
---
+++ password_generator.py
@@ -0,0 +1,56 @@
+import random 
+import string 
+
+def generate_password(min_length, numbers=True, special_characters=True):
+    letters = string.ascii_letters 
+    digits = string.digits
+    special = string.punctuation 
+
+    characters = letters
+    if numbers:
+        characters += digits 
+    if special_characters:
+        characters += special
+
+    meets_criteria = False
+    has_digits = False
+    has_special = False
+
+    password = ''
+
+    while not meets_criteria or len(password) < min_length:
+        new_char = random.choice(characters)
+        password += new_char 
+
+        if numbers and new_char in digits:
+            has_digits = True
+
+        if special_characters and new_char in special:
+            has_special = True
+
+        meets_criteria = True
+
+        if numbers:
+            meets_criteria = meets_criteria and has_digits
+
+        if special_characters:
+            meets_criteria = meets_criteria and has_special
+
+    return password
+
+if __name__=='__main__':
+    min_length = int(input('Enter a Length for your password: '))
+    nums = input('Do you want your password to have numbers? (Yes / NO): ')
+    specials = input('Do you want your password to have special characters? (Yes / No): ')
+
+    if nums.lower() == 'yes' and specials.lower() == 'yes':
+        print(generate_password(min_length))
+    
+    elif nums.lower() == 'yes' and specials.lower() == 'no':
+        print(generate_password(min_length, True, False))
+
+    elif nums.lower() == 'no' and specials.lower() == 'yes':
+        print(generate_password(min_length, False, True))
+
+    else:
+        print(generate_password(min_length, False, False))
